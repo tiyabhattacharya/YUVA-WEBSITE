f8dd848c72c361ef4649f529d52f92f83f104621
tiyabhattacharya <211b328@juetguna.in>
2025-08-13 20:26:40 +0530

    3rd

--- yuva-raipur/src/pages/Home.jsx
+++ yuva-raipur/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 // src/pages/Home.jsx
 import React from "react";
-import Navbar from "../components/Navbar";
+// import Navbar from "../components/Navbar";
 import { Import } from "lucide-react";
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 
 function Home() {
   return (
